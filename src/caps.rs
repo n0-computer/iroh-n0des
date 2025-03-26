d48f65bfff6a546871e37aad6d86b6e419e57634
Frando <frando@unbiskant.org>
2025-03-26 14:43:32 +0100

    refactor: flatten caps structure, add to-and-from string conversions

--- src/caps.rs
+++ src/caps.rs
@@ -1,15 +1,42 @@
-use std::{collections::BTreeSet, time::Duration};
+use std::{collections::BTreeSet, str::FromStr, time::Duration};
 
-use anyhow::{Context, Result};
-use ed25519_dalek::{SigningKey, VerifyingKey};
+use anyhow::{bail, Context, Result};
+use ed25519_dalek::SigningKey;
 use iroh::NodeId;
 use rcan::{Capability, Expires, Rcan};
 use serde::{Deserialize, Serialize};
 use ssh_key::PrivateKey as SshPrivateKey;
 
+macro_rules! cap_enum(
+    ($enum:item) => {
+        #[derive(
+            Debug,
+            Eq,
+            PartialEq,
+            Ord,
+            PartialOrd,
+            Serialize,
+            Deserialize,
+            Clone,
+            Copy,
+            strum::Display,
+            strum::EnumString,
+        )]
+        #[strum(serialize_all = "kebab-case")]
+        #[serde(rename_all = "kebab-case")]
+        $enum
+    }
+);
+
 #[derive(Debug, Eq, PartialEq, Serialize, Deserialize, Clone)]
 pub enum Caps {
     V0(CapSet<Cap>),
+}
+
+impl Default for Caps {
+    fn default() -> Self {
+        Self::V0(CapSet::default())
+    }
 }
 
 impl std::ops::Deref for Caps {
@@ -21,38 +48,90 @@ impl std::ops::Deref for Caps {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize, Clone)]
+#[derive(
+    Debug,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Serialize,
+    Deserialize,
+    Clone,
+    Copy,
+    derive_more::From,
+    strum::Display,
+)]
 pub enum Cap {
+    #[strum(to_string = "all")]
     All,
-    Blobs(CapSet<BlobsCap>),
-    Relay(CapSet<RelayCap>),
-    Metrics(CapSet<MetricsCap>),
+    #[strum(to_string = "blobs:{0}")]
+    Blobs(BlobsCap),
+    #[strum(to_string = "relay:{0}")]
+    Relay(RelayCap),
+    #[strum(to_string = "metrics:{0}")]
+    Metrics(MetricsCap),
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize, Clone)]
-pub enum BlobsCap {
-    All,
-    PutBlob,
-    GetTag,
-}
+impl FromStr for Cap {
+    type Err = anyhow::Error;
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize, Clone)]
-pub enum MetricsCap {
-    PutAny,
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        if s == "all" {
+            Ok(Self::All)
+        } else if let Some((domain, inner)) = s.split_once(":") {
+            Ok(match domain {
+                "blobs" => Self::Blobs(BlobsCap::from_str(inner)?),
+                "metrics" => Self::Metrics(MetricsCap::from_str(inner)?),
+                "relay" => Self::Relay(RelayCap::from_str(inner)?),
+                _ => bail!("invalid cap domain"),
+            })
+        } else {
+            Err(anyhow::anyhow!("invalid cap string"))
+        }
+    }
 }
+cap_enum!(
+    pub enum BlobsCap {
+        All,
+        PutBlob,
+        GetTag,
+    }
+);
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize, Clone)]
-pub enum RelayCap {
-    UseUnlimited,
-}
+cap_enum!(
+    pub enum MetricsCap {
+        PutAny,
+    }
+);
+
+cap_enum!(
+    pub enum RelayCap {
+        UseUnlimited,
+    }
+);
 
 impl Caps {
-    pub fn new(caps: impl IntoIterator<Item = Cap>) -> Self {
+    pub fn new(caps: impl IntoIterator<Item = impl Into<Cap>>) -> Self {
         Self::V0(CapSet::new(caps))
     }
 
     pub fn all() -> Self {
         Self::new([Cap::All])
+    }
+
+    pub fn extend(self, caps: impl IntoIterator<Item = impl Into<Cap>>) -> Self {
+        let Self::V0(mut set) = self;
+        set.extend(caps.into_iter().map(Into::into));
+        Self::V0(set)
+    }
+
+    pub fn from_strs<'a>(strs: impl Iterator<Item = &'a str>) -> Result<Self> {
+        let mut caps = CapSet::default();
+        for s in strs {
+            let cap = Cap::from_str(s).with_context(|| "invalid cap string: {s}")?;
+            caps.insert(cap);
+        }
+        Ok(Self::V0(caps))
     }
 }
 
@@ -67,20 +146,6 @@ impl Capability for Caps {
 impl From<Cap> for Caps {
     fn from(cap: Cap) -> Self {
         Self::new([cap])
-    }
-}
-
-impl Cap {
-    pub fn blobs(set: impl IntoIterator<Item = BlobsCap>) -> Self {
-        Self::Blobs(CapSet::new(set))
-    }
-
-    pub fn metrics(set: impl IntoIterator<Item = MetricsCap>) -> Self {
-        Self::Metrics(CapSet::new(set))
-    }
-
-    pub fn relay(set: impl IntoIterator<Item = RelayCap>) -> Self {
-        Self::Relay(CapSet::new(set))
     }
 }
 
@@ -126,9 +191,15 @@ impl Capability for RelayCap {
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize, Clone)]
 pub struct CapSet<C: Capability + Ord>(BTreeSet<C>);
 
+impl<C: Capability + Ord> Default for CapSet<C> {
+    fn default() -> Self {
+        Self(BTreeSet::new())
+    }
+}
+
 impl<C: Capability + Ord> CapSet<C> {
-    pub fn new(set: impl IntoIterator<Item = C>) -> Self {
-        Self(BTreeSet::from_iter(set))
+    pub fn new(set: impl IntoIterator<Item = impl Into<C>>) -> Self {
+        Self(BTreeSet::from_iter(set.into_iter().map(Into::into)))
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &'_ C> + '_ {
@@ -143,8 +214,17 @@ impl<C: Capability + Ord> CapSet<C> {
         self.0.len()
     }
 
-    pub fn contains(&self, cap: &C) -> bool {
-        self.0.contains(cap)
+    pub fn contains(&self, cap: impl Into<C>) -> bool {
+        let cap = cap.into();
+        self.0.contains(&cap)
+    }
+
+    pub fn extend(&mut self, caps: impl IntoIterator<Item = impl Into<C>>) {
+        self.0.extend(caps.into_iter().map(Into::into));
+    }
+
+    pub fn insert(&mut self, cap: impl Into<C>) -> bool {
+        self.0.insert(cap.into())
     }
 }
 
@@ -171,8 +251,7 @@ pub fn create_api_token(
         .clone()
         .into();
 
-    // TODO: add Into to iroh-base
-    let audience = VerifyingKey::from_bytes(local_node_id.as_bytes())?;
+    let audience = local_node_id.public();
     let can =
         Rcan::issuing_builder(&issuer, audience, capability).sign(Expires::valid_for(max_age));
     Ok(can)
@@ -184,11 +263,26 @@ mod tests {
 
     #[test]
     fn smoke() {
-        let all_listed = Caps::new([
-            Cap::blobs([BlobsCap::PutBlob, BlobsCap::GetTag]),
-            Cap::relay([RelayCap::UseUnlimited]),
-            Cap::metrics([MetricsCap::PutAny]),
-        ]);
+        let all_listed = Caps::default()
+            .extend([BlobsCap::PutBlob, BlobsCap::GetTag])
+            .extend([RelayCap::UseUnlimited])
+            .extend([MetricsCap::PutAny]);
+
+        // test to-and-from string conversion
+        println!("all:     {:?}", all_listed);
+        let strings: Vec<String> = all_listed.iter().map(ToString::to_string).collect();
+        println!("strings: {:?}", strings);
+        let parsed = Caps::from_strs(strings.iter().map(|s| s.as_str())).unwrap();
+        println!("parsed:  {:?}", parsed);
+        assert_eq!(all_listed, parsed);
+
+        // manual parsing from strings
+        let s = ["blobs:put-blob", "relay:use-unlimited"];
+        let caps = Caps::from_strs(s.into_iter()).unwrap();
+        assert_eq!(
+            caps,
+            Caps::new([BlobsCap::PutBlob]).extend([RelayCap::UseUnlimited])
+        );
 
         let all = Caps::new([Cap::All]);
 
@@ -196,9 +290,9 @@ mod tests {
         assert!(all.permits(&all_listed));
         assert!(!all_listed.permits(&all));
 
-        let get_tags = Caps::new([Cap::blobs([BlobsCap::GetTag])]);
-        let put_blobs = Caps::new([Cap::blobs([BlobsCap::PutBlob])]);
-        let relay = Caps::new([Cap::relay([RelayCap::UseUnlimited])]);
+        let get_tags = Caps::new([BlobsCap::GetTag]);
+        let put_blobs = Caps::new([BlobsCap::PutBlob]);
+        let relay = Caps::new([RelayCap::UseUnlimited]);
 
         for cap in [&get_tags, &put_blobs, &relay] {
             assert!(all.permits(&cap));
@@ -210,7 +304,7 @@ mod tests {
         assert!(!get_tags.permits(&put_blobs));
         assert!(!get_tags.permits(&relay));
 
-        let all_blobs = Caps::new([Cap::blobs([BlobsCap::All])]);
+        let all_blobs = Caps::new([BlobsCap::All]);
         assert!(all_blobs.permits(&get_tags));
         assert!(all_blobs.permits(&put_blobs));
         assert!(!put_blobs.permits(&all_blobs));
