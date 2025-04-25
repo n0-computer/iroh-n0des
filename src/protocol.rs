f1548141929c4ecdf7f516d7a3239a6a5a773b18
dignifiedquire <me@dignifiedquire.com>
2025-04-25 12:25:12 +0200

    wip: switch to irpc

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,42 +1,130 @@
+use anyhow::Result;
+use iroh::Endpoint;
 use iroh_blobs::{ticket::BlobTicket, Hash};
+use irpc::{channel::oneshot, Client, Service};
+use irpc_derive::rpc_requests;
+use irpc_iroh::IrohRemoteConnection;
 use rcan::Rcan;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::caps::Caps;
 
-pub const ALPN: &[u8] = b"/iroh/n0des/1";
+#[derive(Debug, Clone, Copy)]
+struct N0desService;
 
-/// Messages sent from the client to the server
-#[allow(clippy::large_enum_variant)]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum ServerMessage {
-    /// Authentication on first request
-    Auth(Rcan<Caps>),
-    /// Request that the node fetches the given blob.
-    PutBlob { ticket: BlobTicket, name: String },
-    /// Request the name of a blob held by the node
-    GetTag { name: String },
-    /// Request to store the given metrics data
-    PutMetrics { encoded: String, session_id: Uuid },
-    /// Simple ping requests
-    Ping { req: [u8; 32] },
+impl Service for N0desService {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Get {
+    key: String,
 }
 
-/// Messages sent from the server to the client
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum ClientMessage {
-    // Empty reply
-    Ack,
-    /// Authentication response
-    /// if set, error, otherwise ok
-    AuthResponse(Option<String>),
-    /// If set, this means it was an error.
-    PutBlobResponse(Option<String>),
-    /// Simple pong response
-    Pong {
-        req: [u8; 32],
-    },
-    // if **missing**, means there was an error
-    GetTagResponse(Option<Hash>),
+#[derive(Debug, Serialize, Deserialize)]
+struct List;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Set {
+    key: String,
+    value: String,
+}
+
+#[rpc_requests(N0desService, message = N0desMessage)]
+#[derive(Serialize, Deserialize)]
+pub enum N0desProtocol {
+    #[rpc(tx=oneshot::Sender<()>)]
+    Auth(Auth),
+    #[rpc(tx=oneshot::Sender<()>)]
+    PutBlob(PutBlob),
+    #[rpc(tx=oneshot::Sender<Option<Hash>>)]
+    GetTag(GetTag),
+    #[rpc(tx=oneshot::Sender<()>)]
+    PutMetrics(PutMetrics),
+    #[rpc(tx=oneshot::Sender<[u8; 32]>)]
+    Ping(Ping),
+}
+
+/// Authentication on first request
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Auth {
+    caps: Rcan<Caps>,
+}
+
+/// Request that the node fetches the given blob.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PutBlob {
+    ticket: BlobTicket,
+    name: String,
+}
+
+/// Request the name of a blob held by the node
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetTag {
+    name: String,
+}
+
+/// Request to store the given metrics data
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PutMetrics {
+    encoded: String,
+    session_id: Uuid,
+}
+
+/// Simple ping requests
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Ping {
+    req: [u8; 32],
+}
+
+pub struct N0desApi {
+    inner: Client<N0desMessage, N0desProtocol, N0desService>,
+}
+
+impl N0desApi {
+    pub const ALPN: &[u8] = b"/iroh/n0des/1";
+
+    pub fn connect(endpoint: Endpoint, addr: impl Into<iroh::NodeAddr>) -> Result<N0desApi> {
+        let conn = IrohRemoteConnection::new(endpoint, addr.into(), Self::ALPN.to_vec());
+        Ok(N0desApi {
+            inner: Client::boxed(conn),
+        })
+    }
+
+    pub async fn auth(&self, caps: Rcan<Caps>) -> Result<(), irpc::Error> {
+        self.inner.rpc(Auth { caps }).await
+    }
+
+    pub async fn put_blob(
+        &self,
+        name: impl AsRef<str>,
+        ticket: BlobTicket,
+    ) -> Result<(), irpc::Error> {
+        self.inner
+            .rpc(PutBlob {
+                name: name.as_ref().to_string(),
+                ticket,
+            })
+            .await
+    }
+
+    pub async fn get_tag(&self, name: impl AsRef<str>) -> Result<Option<Hash>, irpc::Error> {
+        self.inner
+            .rpc(GetTag {
+                name: name.as_ref().to_string(),
+            })
+            .await
+    }
+
+    pub async fn put_metrics(&self, session_id: Uuid, encoded: String) -> Result<(), irpc::Error> {
+        self.inner
+            .rpc(PutMetrics {
+                session_id,
+                encoded,
+            })
+            .await
+    }
+
+    pub async fn ping(&self, req: [u8; 32]) -> Result<[u8; 32], irpc::Error> {
+        self.inner.rpc(Ping { req }).await
+    }
 }
