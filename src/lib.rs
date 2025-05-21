3ca2720dbbc233360d700b6fc1c87fb4dea524c9
b5 <sparkle_pony_2000@qri.io>
2025-05-20 23:11:09 -0400

    feat: N0de trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod client;
+mod n0des;
 mod protocol;
 
 pub mod caps;
 
 pub use self::{
     client::{Client, ClientBuilder},
+    n0des::N0de,
     protocol::{ClientMessage, ServerMessage, ALPN},
 };
