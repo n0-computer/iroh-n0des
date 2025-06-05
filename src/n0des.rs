2d8e6f5d97a31f9e640813ffdd765334e3ca7a88
b5 <sparkle_pony_2000@qri.io>
2025-06-05 11:19:03 -0400

    WIP: getting compilation to work

--- src/n0des.rs
+++ src/n0des.rs
@@ -1,20 +1,14 @@
+use std::future::Future;
+
 use anyhow::Result;
 use iroh::Endpoint;
-use n0_future::Future;
 
+/// A trait for nodes that can be spawned and shut down
 pub trait N0de: 'static + Send {
     fn spawn(endpoint: Endpoint) -> impl Future<Output = Result<Self>> + Send
     where
         Self: Sized;
-    fn shutdown(&mut self) -> impl Future<Output = Result<()>> + Send;
-}
 
-#[macro_export]
-macro_rules! export_node {
-    ($type:ty) => {
-        #[no_mangle]
-        pub extern "C" fn load_plugin() -> Box<dyn $crate::N0de> {
-            Box::new(<$type>::default())
-        }
-    };
+    /// Asynchronously shut down the node
+    fn shutdown(&mut self) -> impl Future<Output = Result<()>> + Send;
 }
