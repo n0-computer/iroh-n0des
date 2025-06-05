a4f78675e1539e6cc53d81d218d0e22ddb068e89
b5 <sparkle_pony_2000@qri.io>
2025-06-04 22:29:31 -0400

    add exprt macro

--- src/n0des.rs
+++ src/n0des.rs
@@ -8,3 +8,13 @@ pub trait N0de: 'static + Send {
         Self: Sized;
     fn shutdown(&mut self) -> impl Future<Output = Result<()>> + Send;
 }
+
+#[macro_export]
+macro_rules! export_node {
+    ($type:ty) => {
+        #[no_mangle]
+        pub extern "C" fn load_plugin() -> Box<dyn $crate::N0de> {
+            Box::new(<$type>::default())
+        }
+    };
+}
