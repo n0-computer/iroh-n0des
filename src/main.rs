dd35db6a0a4b97318e1d4589c624988f2cd521cf
Frando <frando@unbiskant.org>
2025-06-24 14:15:54 +0200

    fix: remove unneeded deps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use iroh::{protocol::Router, Endpoint, NodeAddr, NodeId};
+use iroh::{Endpoint, NodeAddr, NodeId};
 use iroh_n0des::Client;
 use ssh_key::Algorithm;
 use tracing::debug;
@@ -14,12 +14,7 @@ pub async fn main() -> Result<()> {
     println!("SSH Key: {}", alice_ssh_key.public_key().to_openssh()?);
 
     let client_endpoint = Endpoint::builder().discovery_n0().bind().await?;
-    let client_blobs = iroh_blobs::net_protocol::Blobs::memory().build(&client_endpoint);
-
-    let client_router = Router::builder(client_endpoint)
-        .accept(iroh_blobs::ALPN, client_blobs.clone())
-        .spawn();
-    let client_node_id = client_router.endpoint().node_id();
+    let client_node_id = client_endpoint.node_id();
     debug!("local node: {}", client_node_id,);
 
     let remote_node_id: NodeId = std::env::args().nth(1).unwrap().parse()?;
@@ -29,7 +24,7 @@ pub async fn main() -> Result<()> {
     tokio::signal::ctrl_c().await?;
 
     // Create iroh services client
-    let mut rpc_client = Client::builder(client_router.endpoint())
+    let mut rpc_client = Client::builder(&client_endpoint)
         // .metrics_interval(Duration::from_secs(2))
         .ssh_key(&alice_ssh_key)?
         .build(remote_node_addr.clone())
@@ -37,7 +32,7 @@ pub async fn main() -> Result<()> {
 
     rpc_client.ping().await?;
     println!("ping OK");
-    client_router.shutdown().await?;
+    client_endpoint.close().await;
 
     Ok(())
 }
