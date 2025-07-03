7967557f12cbd0c151e3e4b2d1fa5164a11a9ea2
b5 <b5@users.noreply.github.com>
2025-07-03 03:41:00 +0000

    deploy: 2453e60ad344477afcc60fb68a592466b1ef25bb

---
+++ pr/25/docs/iroh_n0des/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"attr":["sim"],"mod":["caps","protocol","simulation"],"struct":["Client","ClientBuilder"],"trait":["N0de"]};
---
+++ pr/25/docs/iroh_n0des/simulation/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Context","Simulation","SimulationBuilder"]};
