n0-computer/iroh-n0des

commit 30924baacbbf4d875ac19af9b97187c98a023170

refactor: allow multiple kinds of nodes, add shared setup data (#29)
## Description

This is a major refactor of the simulation framework in iroh-n0des.

* Support iroh v035 via feature flags
* The simulation builder and runner now has support for a `setup` step, which returns a user-defined struct (must implement `Serialize`, `Deserialize`, `Clone` and `Send`). The setup is only run once per simulation (not once per node). This "setup data" is then available to all nodes both during spawn and in the round callbacks.
* We now support multiple kinds of nodes in a single simulation. You can add the nodes via `Builder::spawn`, which takes a node count (how many nodes are to be spawned) and a `NodeBuilder`.
* The `NodeBuilder` is generic over anything that implements `Spawn`, which is a supertrait over `Node`. The `Node` trait has a `shutdown` method and an optional accessor for the endpoint (which should be impld if this kind of node runs an iroh endpoint). The `Spawn` trait is generic over a `SetupData`, to which it has access via the `SpawnContext`. With this construction, you can have a `Node` that is identical to your production code, while implementing `Spawn` tailored to the simulation's setup (i.e.: `Spawn` is generic over the setup data, while `Node` is not).
* The runner code has been adapted to use more identical code paths between integrated and isolated runs. To achieve this, a `TraceClient` now is always available. When the `N0DES_TRACE_SERVER` variable is unset, a minimal in-process actor is spawned to handle the required client commands (mostly the start and checkpoint waits). This could be expanded down the road to allow saving trace data locally without using a trace server.

## Breaking Changes

The simulation API is breaking in multiple ways. Not listing everything here because it is not yet widely used. See the examples in `tests` how to use the new API.

## Notes & open questions

src/simulation/proto.rs
@@ -1,16 +1,19 @@
-use std::net::SocketAddr;
+use std::{collections::BTreeMap, net::SocketAddr};
 
 use anyhow::Result;
-use iroh::{Endpoint, NodeAddr, NodeId};
+use bytes::Bytes;
 use iroh_metrics::encoding::Update;
-use irpc::{channel::oneshot, rpc_requests, util::make_insecure_client_endpoint};
+use irpc::{WithChannels, channel::oneshot, rpc_requests, util::make_insecure_client_endpoint};
+#[cfg(feature = "iroh_main")]
 use irpc_iroh::IrohRemoteConnection;
+use n0_future::{IterExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime as DateTime;
 use tracing::debug;
 use uuid::Uuid;
 
 use super::{ENV_TRACE_SERVER, ENV_TRACE_SESSION_ID};
+use crate::iroh::{NodeAddr, NodeId};
 
 pub const ALPN: &[u8] = b"/iroh/n0des-sim/1";
 
@@ -20,8 +23,8 @@ pub type RemoteResult<T> = Result<T, RemoteError>;
 pub enum RemoteError {
     #[error("{0}")]
     Other(String),
-    #[error("Trace not found ({0})")]
-    TraceNotFound(Uuid),
+    #[error("Trace not found")]
+    TraceNotFound,
 }
 
 impl RemoteError {
@@ -44,10 +47,14 @@ pub enum TraceProtocol {
     GetSession(GetSession),
     #[rpc(tx=oneshot::Sender<RemoteResult<Uuid>>)]
     InitTrace(InitTrace),
-    #[rpc(tx=oneshot::Sender<RemoteResult<Uuid>>)]
-    StartTrace(StartTrace),
+    #[rpc(tx=oneshot::Sender<RemoteResult<GetTraceResponse>>)]
+    GetTrace(GetTrace),
+    #[rpc(tx=oneshot::Sender<RemoteResult<StartNodeResponse>>)]
+    StartNode(StartNode),
     #[rpc(tx=oneshot::Sender<RemoteResult<()>>)]
-    EndTrace(EndTrace),
+    EndNode(EndNode),
+    #[rpc(tx=oneshot::Sender<RemoteResult<()>>)]
+    CloseTrace(CloseTrace),
     #[rpc(tx=oneshot::Sender<RemoteResult<()>>)]
     PutCheckpoint(PutCheckpoint),
     #[rpc(tx=oneshot::Sender<RemoteResult<()>>)]
@@ -83,47 +90,59 @@ pub struct InitTrace {
     pub info: TraceInfo,
     #[serde(with = "time::serde::rfc3339")]
     pub start_time: DateTime,
+    pub setup_data: Option<Bytes>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CloseTrace {
+    pub trace_id: Uuid,
+    #[serde(with = "time::serde::rfc3339")]
+    pub end_time: DateTime,
+    pub result: Result<(), String>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct TraceInfo {
     pub name: String,
-    pub expected_nodes: Option<u32>,
+    pub node_count: u32,
     pub expected_checkpoints: Option<u64>,
 }
 
 impl TraceInfo {
-    pub fn new(name: impl ToString) -> Self {
+    pub fn new(name: impl ToString, node_count: u32) -> Self {
         Self {
             name: name.to_string(),
-            expected_nodes: None,
+            node_count,
             expected_checkpoints: None,
         }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct StartTrace {
+pub struct GetTrace {
     pub session_id: Uuid,
     pub name: String,
-    pub scope_info: ScopeInfo,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GetTraceResponse {
+    pub trace_id: Uuid,
+    pub info: TraceInfo,
+    pub setup_data: Option<Bytes>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct StartNode {
+    pub trace_id: Uuid,
+    pub node_info: NodeInfo,
     #[serde(with = "time::serde::rfc3339")]
     pub start_time: DateTime,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum ScopeInfo {
-    Integrated(Vec<NodeInfo>),
-    Isolated(NodeInfo),
-}
-
-impl From<&ScopeInfo> for Scope {
-    fn from(value: &ScopeInfo) -> Self {
-        match value {
-            ScopeInfo::Integrated(_) => Scope::Integrated,
-            ScopeInfo::Isolated(info) => Scope::Isolated(info.idx),
-        }
-    }
+pub struct StartNodeResponse {
+    // pub trace_id: Uuid,
+    // pub setup_data: Option<Bytes>,
 }
 
 pub type NodeIdx = u32;
@@ -135,9 +154,9 @@ pub enum Scope {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct EndTrace {
-    pub scope: Scope,
+pub struct EndNode {
     pub trace_id: Uuid,
+    pub node_idx: NodeIdx,
     #[serde(with = "time::serde::rfc3339")]
     pub end_time: DateTime,
     pub result: Result<(), String>,
@@ -145,15 +164,25 @@ pub struct EndTrace {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NodeInfo {
-    pub id: NodeId,
-    pub idx: u32,
+    pub idx: NodeIdx,
+    pub node_id: Option<NodeId>,
     pub label: Option<String>,
+}
+
+impl NodeInfo {
+    pub fn new_empty(idx: NodeIdx) -> Self {
+        Self {
+            idx,
+            node_id: None,
+            label: None,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NodeInfoWithAddr {
     pub info: NodeInfo,
-    pub addr: NodeAddr,
+    pub addr: Option<NodeAddr>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -190,14 +219,12 @@ pub struct PutCheckpoint {
 pub struct WaitCheckpoint {
     pub trace_id: Uuid,
     pub checkpoint_id: CheckpointId,
-    pub required_count: u32,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct WaitStart {
     pub trace_id: Uuid,
     pub info: NodeInfoWithAddr,
-    pub required_count: u32,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -225,6 +252,20 @@ impl TraceClient {
         }
     }
 
+    pub fn from_env_or_local() -> Result<Self> {
+        Ok(Self::from_env()?.unwrap_or_else(Self::local))
+    }
+
+    pub fn local() -> Self {
+        let (tx, rx) = tokio::sync::mpsc::channel(8);
+        LocalActor::spawn(rx);
+        let session_id = Uuid::now_v7();
+        Self {
+            client: irpc::Client::from(tx),
+            session_id,
+        }
+    }
+
     pub fn connect_quinn_insecure(remote: SocketAddr, session_id: Uuid) -> Result<Self> {
         let addr_localhost = "127.0.0.1:0".parse().unwrap();
         let endpoint = make_insecure_client_endpoint(addr_localhost)?;
@@ -240,14 +281,16 @@ impl TraceClient {
         Self { client, session_id }
     }
 
-    pub async fn connect_iroh(remote: NodeId, session_id: Uuid) -> Result<Self> {
-        let endpoint = Endpoint::builder().discovery_local_network().bind().await?;
+    #[cfg(feature = "iroh_main")]
+    pub async fn connect_iroh(remote: iroh::NodeId, session_id: Uuid) -> Result<Self> {
+        let endpoint = iroh::Endpoint::builder().bind().await?;
         Ok(Self::connect_iroh_endpoint(endpoint, remote, session_id))
     }
 
+    #[cfg(feature = "iroh_main")]
     pub fn connect_iroh_endpoint(
-        endpoint: Endpoint,
-        remote: impl Into<NodeAddr>,
+        endpoint: iroh::Endpoint,
+        remote: impl Into<iroh::NodeAddr>,
         session_id: Uuid,
     ) -> Self {
         let conn = IrohRemoteConnection::new(endpoint, remote.into(), ALPN.to_vec());
@@ -255,15 +298,15 @@ impl TraceClient {
         Self { client, session_id }
     }
 
-    #[cfg(test)]
-    pub(crate) async fn init_and_start_trace(&self, name: &str) -> Result<ActiveTrace> {
-        let trace_info = TraceInfo::new(name);
-        self.init_trace(trace_info).await?;
-        self.start_trace(name.to_string(), ScopeInfo::Integrated(Default::default()))
-            .await
+    pub async fn init_and_start_trace(&self, name: &str) -> Result<ActiveTrace> {
+        let trace_info = TraceInfo::new(name, 1);
+        let trace_id = self.init_trace(trace_info, None).await?;
+        let node_info = NodeInfo::new_empty(0);
+        let client = self.start_node(trace_id, node_info).await?;
+        Ok(client)
     }
 
-    pub async fn init_trace(&self, info: TraceInfo) -> Result<()> {
+    pub async fn init_trace(&self, info: TraceInfo, setup_data: Option<Bytes>) -> Result<Uuid> {
         debug!("init trace {info:?}");
         let trace_id = self
             .client
@@ -271,9 +314,37 @@ impl TraceClient {
                 info,
                 session_id: self.session_id,
                 start_time: DateTime::now_utc(),
+                setup_data,
             })
             .await??;
         debug!("init trace {trace_id}: OK");
+        Ok(trace_id)
+    }
+
+    pub async fn get_trace(&self, name: String) -> Result<GetTraceResponse> {
+        debug!("get trace {name}");
+        let data = self
+            .client
+            .rpc(GetTrace {
+                session_id: self.session_id,
+                name,
+            })
+            .await??;
+        debug!(?data, "get trace: OK");
+        Ok(data)
+    }
+
+    pub async fn close_trace(&self, trace_id: Uuid, result: Result<(), String>) -> Result<()> {
+        let end_time = DateTime::now_utc();
+        debug!(%trace_id, ?result, "close trace");
+        self.client
+            .rpc(CloseTrace {
+                trace_id,
+                end_time,
+                result,
+            })
+            .await??;
+        debug!(%trace_id, "close trace: OK");
         Ok(())
     }
 
@@ -282,24 +353,40 @@ impl TraceClient {
         Ok(res)
     }
 
-    pub async fn start_trace(&self, name: String, scope_info: ScopeInfo) -> Result<ActiveTrace> {
+    pub async fn put_logs(
+        &self,
+        trace_id: Uuid,
+        scope: Scope,
+        json_lines: Vec<String>,
+    ) -> Result<()> {
+        self.client
+            .rpc(PutLogs {
+                scope,
+                trace_id,
+                json_lines,
+            })
+            .await??;
+        Ok(())
+    }
+
+    pub async fn start_node(&self, trace_id: Uuid, node_info: NodeInfo) -> Result<ActiveTrace> {
         let start_time = DateTime::now_utc();
-        debug!("start trace {name}");
-        let scope = Scope::from(&scope_info);
-        let trace_id = self
+        let node_idx = node_info.idx;
+        debug!(%trace_id, node_idx, "start node");
+        let res = self
             .client
-            .rpc(StartTrace {
-                session_id: self.session_id,
-                name,
-                scope_info,
+            .rpc(StartNode {
+                trace_id,
+                node_info,
                 start_time,
             })
             .await??;
-        debug!("start trace {trace_id}: OK");
+        let StartNodeResponse {} = res;
+        debug!(%trace_id, node_idx, "start node: OK");
         Ok(ActiveTrace {
             client: self.client.clone(),
             trace_id,
-            scope,
+            node_idx,
         })
     }
 }
@@ -308,23 +395,23 @@ impl TraceClient {
 pub struct ActiveTrace {
     client: irpc::Client<TraceProtocol>,
     trace_id: Uuid,
-    scope: Scope,
+    node_idx: u32,
 }
 
 impl ActiveTrace {
-    pub(crate) async fn put_checkpoint(
+    pub async fn put_checkpoint(
         &self,
         id: CheckpointId,
-        node_idx: NodeIdx,
         label: Option<String>,
         result: Result<(), String>,
     ) -> Result<()> {
+        debug!(id, "put checkpoint");
         let time = DateTime::now_utc();
         self.client
             .rpc(PutCheckpoint {
                 trace_id: self.trace_id,
                 checkpoint_id: id,
-                node_idx,
+                node_idx: self.node_idx,
                 time,
                 label,
                 result,
@@ -333,13 +420,14 @@ impl ActiveTrace {
         Ok(())
     }
 
-    pub(crate) async fn put_metrics(
+    pub async fn put_metrics(
         &self,
         node_id: NodeId,
         checkpoint_id: Option<CheckpointId>,
         metrics: Update,
     ) -> Result<()> {
         let time = DateTime::now_utc();
+        debug!(count = metrics.values.items.len(), "put metrics");
         self.client
             .rpc(PutMetrics {
                 trace_id: self.trace_id,
@@ -352,10 +440,10 @@ impl ActiveTrace {
         Ok(())
     }
 
-    pub(crate) async fn put_logs(&self, json_lines: Vec<String>) -> Result<()> {
+    pub async fn put_logs(&self, json_lines: Vec<String>) -> Result<()> {
         self.client
             .rpc(PutLogs {
-                scope: self.scope,
+                scope: Scope::Isolated(self.node_idx),
                 trace_id: self.trace_id,
                 json_lines,
             })
@@ -363,17 +451,12 @@ impl ActiveTrace {
         Ok(())
     }
 
-    pub(crate) async fn wait_start(
-        &self,
-        info: NodeInfoWithAddr,
-        required_count: u32,
-    ) -> Result<Vec<NodeInfoWithAddr>> {
-        debug!("waiting for {required_count} nodes to confirm start...");
+    pub async fn wait_start(&self, info: NodeInfoWithAddr) -> Result<Vec<NodeInfoWithAddr>> {
+        debug!("waiting for start...");
         let res = self
             .client
             .rpc(WaitStart {
                 info,
-                required_count,
                 trace_id: self.trace_id,
             })
             .await??;
@@ -381,35 +464,176 @@ impl ActiveTrace {
         Ok(res.infos)
     }
 
-    pub(crate) async fn wait_checkpoint(
-        &self,
-        checkpoint_id: CheckpointId,
-        required_count: u32,
-    ) -> Result<()> {
-        debug!("waiting for {required_count} nodes to confirm checkpoint {checkpoint_id}...");
+    pub async fn wait_checkpoint(&self, checkpoint_id: CheckpointId) -> Result<()> {
+        debug!(?checkpoint_id, "waiting for checkpoint...");
         let res = self
             .client
             .rpc(WaitCheckpoint {
                 checkpoint_id,
-                required_count,
                 trace_id: self.trace_id,
             })
             .await;
         res??;
-        debug!("checkpoint {checkpoint_id} confirmed");
+        debug!(?checkpoint_id, "checkpoint confirmed");
         Ok(())
     }
 
-    pub(crate) async fn end_trace(&self, result: Result<(), String>) -> Result<()> {
+    pub async fn end(&self, result: Result<(), String>) -> Result<()> {
+        debug!("end node");
         let end_time = DateTime::now_utc();
         self.client
-            .rpc(EndTrace {
+            .rpc(EndNode {
                 trace_id: self.trace_id,
-                scope: self.scope,
+                node_idx: self.node_idx,
                 end_time,
                 result,
             })
             .await??;
         Ok(())
     }
+}
+
+#[derive(Default)]
+struct LocalActor {
+    traces_by_name: BTreeMap<String, Uuid>,
+    traces: BTreeMap<Uuid, TraceState>,
+}
+
+struct TraceState {
+    init: InitTrace,
+    nodes: Vec<NodeInfoWithAddr>,
+    barrier_start: Vec<oneshot::Sender<RemoteResult<WaitStartResponse>>>,
+    barrier_checkpoint: BTreeMap<CheckpointId, Vec<oneshot::Sender<RemoteResult<()>>>>,
+}
+
+impl TraceState {
+    fn node_count(&self) -> usize {
+        self.init.info.node_count as usize
+    }
+}
+impl LocalActor {
+    pub fn spawn(rx: tokio::sync::mpsc::Receiver<TraceMessage>) {
+        let actor = Self::default();
+        tokio::task::spawn(actor.run(rx));
+    }
+
+    pub async fn run(mut self, mut rx: tokio::sync::mpsc::Receiver<TraceMessage>) {
+        while let Some(message) = rx.recv().await {
+            self.handle_message(message).await;
+        }
+    }
+    async fn handle_message(&mut self, message: TraceMessage) {
+        match message {
+            TraceMessage::GetSession(_msg) => {}
+            TraceMessage::InitTrace(msg) => {
+                let WithChannels { inner, tx, .. } = msg;
+                if self.traces_by_name.contains_key(&inner.info.name) {
+                    return send_err(tx, RemoteError::other("Trace already initialized")).await;
+                }
+                let uuid = Uuid::now_v7();
+                self.traces_by_name.insert(inner.info.name.clone(), uuid);
+                self.traces.insert(
+                    uuid,
+                    TraceState {
+                        init: inner,
+                        nodes: Default::default(),
+                        barrier_start: Default::default(),
+                        barrier_checkpoint: Default::default(),
+                    },
+                );
+                tx.send(Ok(uuid)).await.ok();
+            }
+            TraceMessage::GetTrace(msg) => {
+                let WithChannels { inner, tx, .. } = msg;
+                let GetTrace {
+                    session_id: _,
+                    name,
+                } = inner;
+                let Some((trace_id, info)) = self
+                    .traces_by_name
+                    .get(&name)
+                    .and_then(|trace_id| self.traces.get_key_value(trace_id))
+                else {
+                    return send_err(tx, RemoteError::other("Trace not initialized")).await;
+                };
+                tx.send(Ok(GetTraceResponse {
+                    trace_id: *trace_id,
+                    info: info.init.info.clone(),
+                    setup_data: info.init.setup_data.clone(),
+                }))
+                .await
+                .ok();
+            }
+            TraceMessage::StartNode(msg) => {
+                let WithChannels { inner, tx, .. } = msg;
+                if self.traces.contains_key(&inner.trace_id) {
+                    tx.send(Ok(StartNodeResponse {})).await.ok();
+                } else {
+                    send_err(tx, RemoteError::other("Trace not initialized")).await;
+                }
+            }
+            TraceMessage::EndNode(msg) => {
+                // noop
+                msg.tx.send(Ok(())).await.ok();
+            }
+            TraceMessage::CloseTrace(msg) => {
+                // noop
+                msg.tx.send(Ok(())).await.ok();
+            }
+            TraceMessage::PutCheckpoint(msg) => {
+                // noop
+                msg.tx.send(Ok(())).await.ok();
+            }
+            TraceMessage::PutLogs(msg) => {
+                // noop
+                msg.tx.send(Ok(())).await.ok();
+            }
+            TraceMessage::PutMetrics(msg) => {
+                // noop
+                msg.tx.send(Ok(())).await.ok();
+            }
+            TraceMessage::WaitCheckpoint(msg) => {
+                let WithChannels { inner, tx, .. } = msg;
+                let Some(trace) = self.traces.get_mut(&inner.trace_id) else {
+                    return send_err(tx, RemoteError::TraceNotFound).await;
+                };
+                let node_count = trace.node_count();
+                let barrier = trace
+                    .barrier_checkpoint
+                    .entry(inner.checkpoint_id)
+                    .or_default();
+                barrier.push(tx);
+                debug!(trace_id=%inner.trace_id, checkpoint=inner.checkpoint_id, count=barrier.len(), total=node_count, "wait checkpoint");
+                if barrier.len() == node_count {
+                    debug!(trace_id=%inner.trace_id, checkpoint=inner.checkpoint_id, "release");
+                    barrier
+                        .drain(..)
+                        .map(|tx| tx.send(Ok(())))
+                        .into_unordered_stream()
+                        .count()
+                        .await;
+                }
+            }
+            TraceMessage::WaitStart(msg) => {
+                let WithChannels { inner, tx, .. } = msg;
+                let Some(trace) = self.traces.get_mut(&inner.trace_id) else {
+                    return send_err(tx, RemoteError::TraceNotFound).await;
+                };
+                trace.nodes.push(inner.info);
+                trace.barrier_start.push(tx);
+                if trace.barrier_start.len() == trace.init.info.node_count as usize {
+                    let data = WaitStartResponse {
+                        infos: trace.nodes.clone(),
+                    };
+                    for tx in trace.barrier_start.drain(..) {
+                        tx.send(Ok(data.clone())).await.ok();
+                    }
+                }
+            }
+        }
+    }
+}
+
+async fn send_err<T>(tx: oneshot::Sender<RemoteResult<T>>, err: RemoteError) {
+    tx.send(Err(err)).await.ok();
 }

commit 3a484525752c149daab1b1ed537bca9bbad26e17

fix: don't close traces in init-only mode (#30)
## Description

`isolated` mode is broken after #29 because we called `close_trace` even when in InitOnly mode. this is wrong of course, close should only be called in integrated mode at the end. When in isolated or init-only mode, close should not be called at all, it is called by the `n0des` cli command after all isolated nodes finished.

Longer explanation what `InitOnly` means (copied from discord):

when running n0des run-sim --isolated,  cargo test first invoked with N0DES_INIT_ONLY=1.  when this is found, RunMode::InitOnly is passed to the simulation builder/runner, which then only sends the InitTrace command to the trace server, which contains the trace names, expected number of nodes, and the setup data. it then exits without running any simulations. the n0des command then gets this data from the trace server, to know how many instances should be launched. these then get an N0DES_ISOLATED_IDX var, which sets RunMode::Isolated(idx), which makes the runner call GetTrace to the server to get the setup data, and then run only a single node. 
(if neither env vars are set, then RunMode::Integrated is invoked, which calls both InitTrace and then runs all nodes in-process)

unrelated change: renamed `spawners` to `node_builders`  for consistency.

## Breaking Changes

TraceClient::status renamed to TraceClient::get_session

src/simulation.rs
@@ -342,7 +342,7 @@ impl<T: Node + Sized> DynNode for T {
 /// before building the final simulation.
 pub struct Builder<D = ()> {
     setup_fn: BoxedSetupFn<D>,
-    spawners: Vec<(u32, ErasedNodeBuilder<D>)>,
+    node_builders: Vec<(u32, ErasedNodeBuilder<D>)>,
     rounds: u32,
 }
 
@@ -612,7 +612,7 @@ impl Builder<()> {
     pub fn new() -> Builder<()> {
         let setup_fn: BoxedSetupFn<()> = Box::new(move || Box::pin(async move { Ok(()) }));
         Builder {
-            spawners: Vec::new(),
+            node_builders: Vec::new(),
             setup_fn,
             rounds: 0,
         }
@@ -638,7 +638,7 @@ impl<D: SetupData> Builder<D> {
     {
         let setup_fn: BoxedSetupFn<D> = Box::new(move || Box::pin(setup_fn()));
         Builder {
-            spawners: Vec::new(),
+            node_builders: Vec::new(),
             setup_fn,
             rounds: 0,
         }
@@ -663,7 +663,7 @@ impl<D: SetupData> Builder<D> {
         node_builder: impl Into<NodeBuilder<N, D>>,
     ) -> Self {
         let node_builder = node_builder.into();
-        self.spawners.push((node_count, node_builder.erase()));
+        self.node_builders.push((node_count, node_builder.erase()));
         self
     }
 
@@ -686,7 +686,11 @@ impl<D: SetupData> Builder<D> {
         {
             let setup_data = (self.setup_fn)().await?;
             let encoded_setup_data = Bytes::from(postcard::to_stdvec(&setup_data)?);
-            let node_count = self.spawners.iter().map(|(count, _spawner)| count).sum();
+            let node_count = self
+                .node_builders
+                .iter()
+                .map(|(count, _spawner)| count)
+                .sum();
             let trace_id = client
                 .init_trace(
                     TraceInfo {
@@ -714,18 +718,18 @@ impl<D: SetupData> Builder<D> {
             (trace_id, setup_data)
         };
 
-        let mut spawners = self
-            .spawners
+        let mut node_builders = self
+            .node_builders
             .into_iter()
             .flat_map(|(count, spawner)| (0..count).map(move |_| spawner.clone()))
             .enumerate()
             .map(|(idx, spawner)| (idx as u32, spawner));
 
-        let spawners: Vec<_> = match run_mode {
+        let node_builders: Vec<_> = match run_mode {
             RunMode::InitOnly => vec![],
-            RunMode::Integrated => spawners.collect(),
+            RunMode::Integrated => node_builders.collect(),
             RunMode::Isolated(idx) => vec![
-                spawners
+                node_builders
                     .nth(idx as usize)
                     .context("invalid isolated index")?,
             ],
@@ -734,7 +738,7 @@ impl<D: SetupData> Builder<D> {
         Ok(Simulation {
             run_mode,
             max_rounds: self.rounds,
-            node_builders: spawners,
+            node_builders,
             client,
             trace_id,
             setup_data,
@@ -768,28 +772,21 @@ impl<D: SetupData> Simulation<D> {
         let cancel_token = CancellationToken::new();
 
         // Spawn a task to submit logs.
-        let logs_task = {
-            let cancel_token = cancel_token.clone();
-            let client = self.client.clone();
-            let trace_id = self.trace_id;
-            let scope = match self.run_mode {
-                RunMode::Isolated(idx) => Scope::Isolated(idx),
-                _ => Scope::Integrated,
-            };
-            tokio::task::spawn(async move {
-                loop {
-                    let _ = cancel_token
-                        .run_until_cancelled(tokio::time::sleep(Duration::from_secs(1)))
-                        .await;
-                    let lines = self::trace::get_logs().await;
-                    if client.put_logs(trace_id, scope, lines).await.is_err() {
-                        break;
-                    }
-                    if cancel_token.is_cancelled() {
-                        break;
-                    }
-                }
-            })
+        let logs_scope = match self.run_mode {
+            RunMode::Isolated(idx) => Some(Scope::Isolated(idx)),
+            RunMode::Integrated => Some(Scope::Integrated),
+            // Do not push logs for init-only runs.
+            RunMode::InitOnly => None,
+        };
+        let logs_task = if let Some(scope) = logs_scope {
+            Some(spawn_logs_task(
+                self.client.clone(),
+                self.trace_id,
+                scope,
+                cancel_token.clone(),
+            ))
+        } else {
+            None
         };
 
         // Spawn and run all nodes concurrently.
@@ -814,9 +811,11 @@ impl<D: SetupData> Simulation<D> {
             .map(|_list| ());
 
         cancel_token.cancel();
-        logs_task.await?;
+        if let Some(join_handle) = logs_task {
+            join_handle.await?;
+        }
 
-        if matches!(self.run_mode, RunMode::InitOnly | RunMode::Integrated) {
+        if matches!(self.run_mode, RunMode::Integrated) {
             self.client
                 .close_trace(self.trace_id, to_str_err(&result))
                 .await?;
@@ -849,6 +848,30 @@ impl RunMode {
             }
         }
     }
+}
+
+/// Spawns a task that periodically submits the collected logs from our global
+/// tracing subscriber to a trace server.
+fn spawn_logs_task(
+    client: TraceClient,
+    trace_id: Uuid,
+    scope: Scope,
+    cancel_token: CancellationToken,
+) -> tokio::task::JoinHandle<()> {
+    tokio::task::spawn(async move {
+        loop {
+            let _ = cancel_token
+                .run_until_cancelled(tokio::time::sleep(Duration::from_secs(1)))
+                .await;
+            let lines = self::trace::get_logs().await;
+            if client.put_logs(trace_id, scope, lines).await.is_err() {
+                break;
+            }
+            if cancel_token.is_cancelled() {
+                break;
+            }
+        }
+    })
 }
 
 static PERMIT: Semaphore = Semaphore::const_new(1);

src/simulation/proto.rs
@@ -348,7 +348,7 @@ impl TraceClient {
         Ok(())
     }
 
-    pub async fn status(&self, session_id: Uuid) -> Result<Option<GetSessionResponse>> {
+    pub async fn get_session(&self, session_id: Uuid) -> Result<Option<GetSessionResponse>> {
         let res = self.client.rpc(GetSession { session_id }).await??;
         Ok(res)
     }